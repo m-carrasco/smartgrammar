a55fd59dd095ece4f8da2380de94b1dbdf722de1
Manuel Carrasco <mgcarrasco2012@gmail.com>
2016-12-20 01:14:08 -0300

    New test for parser. Removed lexer test it is outdated and it is not quite useful if we can test the parser.

--- tests/parserTest.py
+++ tests/parserTest.py
@@ -4,7 +4,6 @@ import src.expressions as exp
 
 import unittest
 
-# Theses are lexer test. They don't necessary have a correct semantic
 class TestParser(unittest.TestCase):
 
     def test_1(self):
@@ -57,6 +56,54 @@ class TestParser(unittest.TestCase):
         self.assertEqual(script.target.paramY,[exp.Number(0,'int')])
         self.assertEqual(script.target.paramZ,[exp.Number(0,'int')]) 
         self.assertEqual(script.target.paramO,[exp.Number(0,'int')])
+
+    def test_2(self):
+        inputScript = """
+            CREATURE SCRIPT: ENTRY=11897
+            AT SMART_EVENT_UPDATE_IC
+                eventId=0
+                param1=5000
+                param2=8000
+                param3=12000
+                param4=15000
+            DO SMART_ACTION_CAST
+                param1=77522
+            ON SMART_TARGET_VICTIM
+            AT SMART_EVENT_RANGE
+                eventId=1
+                param1=0
+                param2=8
+                param3=15000
+                param4=25000
+            DO SMART_ACTION_CAST
+                param1=8281
+            ON SMART_TARGET_SELF
+        """
+        script = parser.parse(inputScript)[0]
+
+        self.assertEqual(script.entry.value, 11897)
+        self.assertEqual(script.event.eventType,{'name': 'SMART_EVENT_UPDATE_IC', 'value': 0})
+        self.assertEqual(script.event.eventId,[exp.Number(0,'int')])
+        self.assertEqual(script.event.param1,[exp.Number(5000,'int')])        
+        self.assertEqual(script.event.param2,[exp.Number(8000,'int')])
+        self.assertEqual(script.event.param3,[exp.Number(12000,'int')])
+        self.assertEqual(script.event.param4,[exp.Number(15000,'int')])
+        self.assertEqual(script.action.actionType, {'name': 'SMART_ACTION_CAST', 'value': 11})
+        self.assertEqual(script.action.param1,[exp.Number(77522,'int')])
+        self.assertEqual(script.target.targetType, {'name': 'SMART_TARGET_VICTIM', 'value': 2})
+
+        script = parser.parse(inputScript)[1]
+
+        self.assertEqual(script.entry.value, 11897)
+        self.assertEqual(script.event.eventType,{'name': 'SMART_EVENT_RANGE', 'value': 9})
+        self.assertEqual(script.event.eventId,[exp.Number(1,'int')])
+        self.assertEqual(script.event.param1,[exp.Number(0,'int')])        
+        self.assertEqual(script.event.param2,[exp.Number(8,'int')])
+        self.assertEqual(script.event.param3,[exp.Number(15000,'int')])
+        self.assertEqual(script.event.param4,[exp.Number(25000,'int')])
+        self.assertEqual(script.action.actionType, {'name': 'SMART_ACTION_CAST', 'value': 11})
+        self.assertEqual(script.action.param1,[exp.Number(8281,'int')])
+        self.assertEqual(script.target.targetType, {'name': 'SMART_TARGET_SELF', 'value': 1})
 
 if __name__ == '__main__':
     unittest.main()
