e9bd11d7615c3845de986e4ba2ab795182d61d8e
Manuel Carrasco <mgcarrasco2012@gmail.com>
2016-12-23 01:39:09 -0300

    Added main file that receives as input a file and generates sqls.

---
+++ main.py
@@ -0,0 +1,21 @@
+from src import parser
+import argparse
+
+p = argparse.ArgumentParser()
+p.add_argument("inputFile", help="file to be processed")
+args = p.parse_args()
+
+f = open(args.inputFile)
+try:
+	lines = f.readlines()
+	# find a nicer way to read the entire file
+	content = "".join(lines)
+	
+	content = parser.parse(content)
+
+	sqls = map(lambda s: s.toSQL(), content)
+	for sql in sqls:
+		print(sql + '\n')
+
+finally:
+    f.close()
